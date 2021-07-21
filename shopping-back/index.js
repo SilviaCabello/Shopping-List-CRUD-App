64629578d20de419481342bc9095b5db99058859
SilviaCabello <silviacabellom@gmail.com>
2021-07-21 13:38:22 +0200

    create routes folder and items-route.js

--- shopping-back/index.js
+++ shopping-back/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
+const itemsRoute = require("./routes/items-route");
+
+app.use("/items", itemsRoute);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
