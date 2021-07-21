cfda5c14e3539be746b2b61b335f036b4e6408ab
SilviaCabello <silviacabellom@gmail.com>
2021-07-21 15:21:27 +0200

    fetch the data from the backend

--- shopping-front/src/App.js
+++ shopping-front/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: [],
+    };
+  }
+
+  fetchItem = () => {
+    fetch("/items")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+
+  componentDidMount() {
+    this.fetchItem();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="text" placeholder="name" />
+        <button>Add</button>
+      </div>
+    );
+  }
 }
 
 export default App;
