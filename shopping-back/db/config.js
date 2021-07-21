4c014aee58f48199b38d8dc70815a8e4b1b556d8
SilviaCabello <silviacabellom@gmail.com>
2021-07-21 13:44:19 +0200

    create config file

---
+++ shopping-back/db/config.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "silvia",
+  password: "Adminpass123!",
+  database: "shoppinglist",
+});
+
+module.exports = connection;
